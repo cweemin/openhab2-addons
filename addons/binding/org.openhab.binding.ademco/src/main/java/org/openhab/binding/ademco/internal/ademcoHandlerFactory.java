6ea3f94ee1a6c7a3d8ea980aede7500c9a8c2e67
WeeMin Chan <cweemin@gmail.com>
2018-08-09 09:24:03 -0700

    Added ademco
    - Print out status if it's not 200
    - Moved response warning to inside when it's not 200
    - Added alarm and check action for Zone
    - switch led icon for arm,stay,instant and chime to be writable
    - Added support for LOBAT message

---
+++ addons/binding/org.openhab.binding.ademco/src/main/java/org/openhab/binding/ademco/internal/ademcoHandlerFactory.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2010-2018 by the respective copyright holders.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.openhab.binding.ademco.internal;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.smarthome.config.discovery.DiscoveryService;
+import org.eclipse.smarthome.core.thing.Bridge;
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingTypeUID;
+import org.eclipse.smarthome.core.thing.ThingUID;
+import org.eclipse.smarthome.core.thing.binding.BaseThingHandlerFactory;
+import org.eclipse.smarthome.core.thing.binding.ThingHandler;
+import org.eclipse.smarthome.core.thing.binding.ThingHandlerFactory;
+import org.openhab.binding.ademco.internal.discovery.AdemcoDiscoveryService;
+import org.openhab.binding.ademco.internal.handler.EnvisalinkBridgeHandler;
+import org.openhab.binding.ademco.internal.handler.PartitionBridgeHandler;
+import org.openhab.binding.ademco.internal.handler.ZoneThingHandler;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The {@link ademcoHandlerFactory} is responsible for creating things and thing
+ * handlers.
+ *
+ * @author WeeMin Chan - Initial contribution
+ */
+@NonNullByDefault
+@Component(configurationPid = "binding.ademco", service = ThingHandlerFactory.class)
+public class ademcoHandlerFactory extends BaseThingHandlerFactory {
+    private Logger logger = LoggerFactory.getLogger(BaseThingHandlerFactory.class);
+    private final Map<ThingUID, ServiceRegistration<?>> discoveryServiceRegistrations = new HashMap<>();
+
+    @Override
+    public boolean supportsThingType(ThingTypeUID thingTypeUID) {
+        if (thingTypeUID.equals(AdemcoConstants.ENVISALINKBRIDGE_THING_TYPE)) {
+            return true;
+        }
+        return AdemcoConstants.BINDING_ID.equals(thingTypeUID.getBindingId());
+    }
+
+    @Override
+    protected @Nullable ThingHandler createHandler(Thing thing) {
+        logger.debug("Try Creating thing {}", thing.getUID());
+        ThingTypeUID thingTypeUID = thing.getThingTypeUID();
+        if (thingTypeUID.equals(AdemcoConstants.ENVISALINKBRIDGE_THING_TYPE)) {
+            logger.debug("Creating Envisalink Bridge");
+            EnvisalinkBridgeHandler handler = new EnvisalinkBridgeHandler((Bridge) thing);
+            this.registerAdemcoAlarmDiscoveryService(handler);
+            return handler;
+        } else if (thingTypeUID.equals(AdemcoConstants.PARTITION_THING_TYPE)) {
+            return new PartitionBridgeHandler((Bridge) thing);
+        } else if (thingTypeUID.equals(AdemcoConstants.ZONE_THING_TYPE)) {
+            return new ZoneThingHandler(thing);
+        }
+        // Everything else gets handled in a single handler
+        return null;
+
+    }
+
+    /**
+     * Register the Thing Discovery Service for a bridge.
+     *
+     * @param AlarmBridgeHandler
+     */
+    private void registerAdemcoAlarmDiscoveryService(EnvisalinkBridgeHandler envisalinkAlarmBridgeHandler) {
+        AdemcoDiscoveryService discoveryService = new AdemcoDiscoveryService(envisalinkAlarmBridgeHandler);
+        discoveryService.activate();
+
+        discoveryServiceRegistrations.put(envisalinkAlarmBridgeHandler.getThing().getUID(),
+                bundleContext.registerService(DiscoveryService.class.getName(), discoveryService, new Hashtable<>()));
+
+        logger.debug("registerDSCAlarmDiscoveryService(): Bridge Handler - {}, Class Name - {}, Discovery Service - {}",
+                envisalinkAlarmBridgeHandler, DiscoveryService.class.getName(), discoveryService);
+    }
+
+    @SuppressWarnings("null")
+    @Override
+    protected synchronized void removeHandler(ThingHandler thingHandler) {
+        logger.debug("removeHandler called");
+        if (thingHandler instanceof EnvisalinkBridgeHandler) {
+            ServiceRegistration<?> serviceReg = this.discoveryServiceRegistrations
+                    .get(thingHandler.getThing().getUID());
+            if (serviceReg != null) {
+                // remove discovery service, if bridge handler is removed
+                AdemcoDiscoveryService service = (AdemcoDiscoveryService) bundleContext
+                        .getService(serviceReg.getReference());
+                if (service != null) {
+                    service.deactivate();
+                }
+                serviceReg.unregister();
+                discoveryServiceRegistrations.remove(thingHandler.getThing().getUID());
+            }
+        }
+    }
+}
